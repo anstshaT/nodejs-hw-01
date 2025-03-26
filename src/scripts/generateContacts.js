b50bbe1f1bddac80392c632a9c7f5ed134b4841c
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-03-26 12:49:17 +0100

    Update all files to final step

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,11 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contactsList = await readContacts();
+  const newContactsList = Array(number).fill(0).map(createFakeContact);
+  await writeContacts([...contactsList, ...newContactsList]);
+};
 
 generateContacts(5);
